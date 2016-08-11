137bdc12aa6e582ef75d044d11b2bfa464c09732
c457 <android.c357@gmail.com>
2016-08-11 10:32:18 -0500

    venus: cleanup ramdisk

--- ramdisk/ramdisk_venus.mk
+++ ramdisk/ramdisk_venus.mk
@@ -3,58 +3,28 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/fstab.hi6250:root/fstab.hi6250 \
 	$(LOCAL_PATH)/fstab.zram256m:root/fstab.zram256m \
 	$(LOCAL_PATH)/fstab.zram512m:root/fstab.zram512m \
+	$(LOCAL_PATH)/init.51312.rc:root/init.51312.rc \
+	$(LOCAL_PATH)/init.audio.rc:root/init.audio.rc \
+	$(LOCAL_PATH)/init.balong_modem.rc:root/init.balong_modem.rc \
+	$(LOCAL_PATH)/init.connectivity.hisi.rc:root/init.connectivity.hisi.rc \
+	$(LOCAL_PATH)/init.connectivity.rc:root/init.connectivity.rc \
+	$(LOCAL_PATH)/init.device.rc:root/init.device.rc \
+	$(LOCAL_PATH)/init.extmodem.rc:root/init.extmodem.rc \
 	$(LOCAL_PATH)/init.hi6250.rc:root/init.hi6250.rc \
+	$(LOCAL_PATH)/init.hisi.rc:root/init.hisi.rc \
+	$(LOCAL_PATH)/init.manufacture.rc:root/init.manufacture.rc \
+	$(LOCAL_PATH)/init.platform.rc:root/init.platform.rc \
+	$(LOCAL_PATH)/init.post-fs-data.rc:root/init.post-fs-data.rc \
+	$(LOCAL_PATH)/init.rc:root/init.rc \
+	$(LOCAL_PATH)/init.tee.rc:root/init.tee.rc \
+	$(LOCAL_PATH)/init.usb.rc:root/init.usb.rc \
+	$(LOCAL_PATH)/resetFactory.cfg:root/resetFactory.cfg \
+	$(LOCAL_PATH)/sbin/hdbd:root/sbin/hdbd \
+	$(LOCAL_PATH)/sbin/hw_crit_service_sys:root/sbin/hw_crit_service_sys \
+	$(LOCAL_PATH)/sbin/hw_ueventd:root/sbin/hw_ueventd \
+	$(LOCAL_PATH)/sbin/kmsgcat:root/sbin/kmsgcat \
+	$(LOCAL_PATH)/sbin/ntfs-3gd:root/sbin/ntfs-3gd \
+	$(LOCAL_PATH)/sbin/oeminfo_nvm_server:root/sbin/oeminfo_nvm_server \
 	$(LOCAL_PATH)/ueventd.hi6250.rc:root/ueventd.hi6250.rc \
-	$(LOCAL_PATH)/init:root/init\
-	$(LOCAL_PATH)/init.4871.rc:root/init.4871.rc\
-	$(LOCAL_PATH)/init.6193.rc:root/init.6193.rc\
-	$(LOCAL_PATH)/init.51312.rc:root/init.51312.rc\
-	$(LOCAL_PATH)/init.audio.rc:root/init.audio.rc\
-	$(LOCAL_PATH)/init.balong_modem.rc:root/init.balong_modem.rc\
-	$(LOCAL_PATH)/init.chip.usb.rc:root/init.chip.usb.rc\
-	$(LOCAL_PATH)/init.connectivity.bcm43xx.rc:root/init.connectivity.bcm43xx.rc\
-	$(LOCAL_PATH)/init.connectivity.bcm43455.rc:root/init.connectivity.bcm43455.rc\
-	$(LOCAL_PATH)/init.connectivity.gps.rc:root/init.connectivity.gps.rc\
-	$(LOCAL_PATH)/init.connectivity.hisi.rc:root/init.connectivity.hisi.rc\
-	$(LOCAL_PATH)/init.connectivity.rc:root/init.connectivity.rc\
-	$(LOCAL_PATH)/init.device.rc:root/init.device.rc\
-	$(LOCAL_PATH)/init.environ.rc:root/init.environ.rc\
-	$(LOCAL_PATH)/init.extmodem.rc:root/init.extmodem.rc\
-	$(LOCAL_PATH)/init.hi6250.rc:root/init.hi6250.rc\
-	$(LOCAL_PATH)/init.hisi.rc:root/init.hisi.rc\
-	$(LOCAL_PATH)/init.manufacture.rc:root/init.manufacture.rc\
-	$(LOCAL_PATH)/init.performance.rc:root/init.performance.rc\
-	$(LOCAL_PATH)/init.platform.rc:root/init.platform.rc\
-	$(LOCAL_PATH)/init.post-fs-data.rc:root/init.post-fs-data.rc\
-	$(LOCAL_PATH)/init.protocol.rc:root/init.protocol.rc\
-	$(LOCAL_PATH)/init.rc:root/init.rc\
-	$(LOCAL_PATH)/init.recovery.balong_modem.rc:root/init.recovery.balong_modem.rc\
-	$(LOCAL_PATH)/init.recovery.hisi.rc:root/init.recovery.hisi.rc\
-	$(LOCAL_PATH)/init.recovery.huawei.rc:root/init.recovery.huawei.rc\
-	$(LOCAL_PATH)/init.tee.rc:root/init.tee.rc\
-	$(LOCAL_PATH)/init.trace.rc:root/init.trace.rc\
-	$(LOCAL_PATH)/init.usb.rc:root/init.usb.rc\
-	$(LOCAL_PATH)/init.zygote32.rc:root/init.zygote32.rc\
-	$(LOCAL_PATH)/init.zygote64_32.rc:root/init.zygote64_32.rc\
-	$(LOCAL_PATH)/property_contexts:root/property_contexts\
-	$(LOCAL_PATH)/resetFactory.cfg:root/resetFactory.cfg\
-	$(LOCAL_PATH)/seapp_contexts:root/seapp_contexts\
-	$(LOCAL_PATH)/selinux_version:root/selinux_version\
-	$(LOCAL_PATH)/sepolicy:root/sepolicy\
-	$(LOCAL_PATH)/service_contexts:root/service_contexts\
-	$(LOCAL_PATH)/ueventd.4871.rc:root/ueventd.4871.rc\
-	$(LOCAL_PATH)/ueventd.6193.rc:root/ueventd.6193.rc\
-	$(LOCAL_PATH)/ueventd.51312.rc:root/ueventd.51312.rc\
-	$(LOCAL_PATH)/ueventd.hi6250.rc:root/ueventd.hi6250.rc\
-	$(LOCAL_PATH)/ueventd.rc:root/ueventd.rc\
-	$(LOCAL_PATH)/sbin/adbd:root/sbin/adbd\
-	$(LOCAL_PATH)/sbin/check_root:root/sbin/check_root\
-	$(LOCAL_PATH)/sbin/hdbd:root/sbin/hdbd\
-	$(LOCAL_PATH)/sbin/healthd:root/sbin/healthd\
-	$(LOCAL_PATH)/sbin/hw_crit_service_sys:root/sbin/hw_crit_service_sys\
-	$(LOCAL_PATH)/sbin/hw_ueventd:root/sbin/hw_ueventd\
-	$(LOCAL_PATH)/sbin/kmsgcat:root/sbin/kmsgcat\
-	$(LOCAL_PATH)/sbin/launch_daemonsu.sh:root/sbin/launch_daemonsu.sh\
-	$(LOCAL_PATH)/sbin/logctl_service:root/sbin/logctl_service\
-	$(LOCAL_PATH)/sbin/ntfs-3gd:root/sbin/ntfs-3gd\
-	$(LOCAL_PATH)/sbin/oeminfo_nvm_server:root/sbin/oeminfo_nvm_server
+	$(LOCAL_PATH)/ueventd.rc:root/ueventd.rc
+
